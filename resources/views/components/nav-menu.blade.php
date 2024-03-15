57ebf89955ae8e192d3a25312c3a58c3705e7428
Niels <44090733+NielsKoomans97@users.noreply.github.com>
2024-03-15 13:51:29 +0100

    Button styling

--- resources/views/components/nav-menu.blade.php
+++ resources/views/components/nav-menu.blade.php
@@ -1,7 +1,8 @@
 <section class="nav-menu">
     <div class="container">
+        <img class="logo" src="{{ $logo }}">
         <nav>
-            <x-button icon="fa-home" direction="icon-first" href="{{ url('/') }}">Home</x-button>
+            <x-button class="secondary" icon="fa-home" direction="icon-first" href="{{ url('/') }}">Home</x-button>
         </nav>
     </div>
 </section>
